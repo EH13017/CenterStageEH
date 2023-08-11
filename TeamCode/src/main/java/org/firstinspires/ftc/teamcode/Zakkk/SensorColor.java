14ba7ba2bc02644127b4fae585f089b8f85a9592
Obstagoon2 <Zakkljackson@hotmail.com>
2023-08-10 20:02:32 -0500

    testing color sensor 8-10-2023 8:00pm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zakkk/SensorColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zakkk/SensorColor.java
@@ -8,6 +8,8 @@ import android.view.View;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
@@ -48,6 +50,19 @@ public class SensorColor extends LinearOpMode {
     /** The colorSensor field will contain a reference to our color sensor hardware object */
     NormalizedColorSensor colorSensor;
 
+
+
+    // Wheels
+    private DcMotor WheelFrontLeft;
+    private DcMotor WheelFrontRight;
+    private DcMotor WheelBackLeft;
+    private DcMotor WheelBackRight;
+    // Sine
+    private double modifyBySine = Math.sin(Math.PI/4);
+    private final double SLOW_DRIVE = 0.4;
+    private double percentToSlowDrive = SLOW_DRIVE;
+
+
     /** The relativeLayout field is used to aid in providing interesting visual feedback
      * in this sample application; you probably *don't* need this when you use a color sensor on your
      * robot. Note that you won't see anything change on the Driver Station, only on the Robot Controller. */
@@ -69,6 +84,32 @@ public class SensorColor extends LinearOpMode {
         // color of the Robot Controller app to match the hue detected by the RGB sensor.
         int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
         relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
+
+        WheelFrontLeft = hardwareMap.dcMotor.get("WheelFL");
+        WheelFrontRight = hardwareMap.dcMotor.get("WheelFR");
+        WheelBackLeft = hardwareMap.dcMotor.get("WheelBL");
+        WheelBackRight = hardwareMap.dcMotor.get("WheelBR");
+
+        WheelFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        WheelFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        WheelBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        WheelBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        WheelFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        WheelFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        WheelBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        WheelBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        WheelFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        WheelFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        WheelBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        WheelBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        WheelFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        WheelFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        WheelBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        WheelBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         try {
             runSample(); // actually execute the sample
@@ -143,7 +184,7 @@ public class SensorColor extends LinearOpMode {
             colorSensor.setGain(gain);
 
 
-            // Check the status of the X button on the gamepad
+           /* // Check the status of the X button on the gamepad
             xButtonCurrentlyPressed = gamepad1.y;
 
             if (xButtonCurrentlyPressed) {
@@ -172,7 +213,7 @@ public class SensorColor extends LinearOpMode {
                 }
             } else {
                 telemetry.addData("Compare Y", "The Same");
-            }
+            }*/
             xButtonPreviouslyPressed = xButtonCurrentlyPressed;
 
             // Get the normalized colors from the sensor
@@ -212,6 +253,35 @@ public class SensorColor extends LinearOpMode {
                 }
             });
         }
+    }
+    //https://ftcforum.usfirst.org/forum/ftc-technology/android-studio/6361-mecanum-wheels-drive-code-example
+    //******************************************************************
+    // Get the inputs from the controller for power [ PRO ]
+    //******************************************************************
+    private void ProMotorControl(double left_stick_y, double left_stick_x, double right_stick_x) {
+        double powerLeftY = -left_stick_y;   // DRIVE : Backward -1 <---> 1 Forward
+        double powerLeftX = left_stick_x*-1; // STRAFE:     Left -1 <---> 1 Right
+        double powerRightX = -right_stick_x; // ROTATE:     Left -1 <---> 1 Right
+
+        double r = Math.hypot(powerLeftX, powerLeftY);
+        double robotAngle = Math.atan2(powerLeftY, powerLeftX) - Math.PI / 4;
+        double leftX = powerRightX;
+
+        final double v1 = r * Math.cos(robotAngle) / modifyBySine + leftX;
+        final double v2 = r * Math.sin(robotAngle) / modifyBySine - leftX;
+        final double v3 = r * Math.sin(robotAngle) / modifyBySine + leftX;
+        final double v4 = r * Math.cos(robotAngle) / modifyBySine - leftX;
+
+
+        WheelFrontLeft.setPower(v1* percentToSlowDrive);
+        WheelFrontRight.setPower(v2* percentToSlowDrive);
+        WheelBackLeft.setPower(v3* percentToSlowDrive);
+        WheelBackRight.setPower(v4* percentToSlowDrive);
+
+        telemetry.addData("Wheel Front Left",v1* percentToSlowDrive);
+        telemetry.addData("Wheel Front Right",v2* percentToSlowDrive);
+        telemetry.addData("Wheel Back Left",v3* percentToSlowDrive);
+        telemetry.addData("Wheel Back Right",v4* percentToSlowDrive);
     }
 }
 
