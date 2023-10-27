623039dfd3d6a14c3722339fc3b28092e9a2fcaf
Obstagoon2 <Zakkljackson@hotmail.com>
2023-10-27 14:51:44 -0500

    Version 9.0 (20230830-154348)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zakkk/zakkktest.java
@@ -0,0 +1,166 @@
+/*package org.firstinspires.ftc.teamcode.comp;
+
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Interface.IDrive;
+
+/*
+ * This OpMode illustrates how to use the Modern Robotics Range Sensor.
+ *
+ * The OpMode assumes that the range sensor is configured with a name of "sensor_range".
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ *
+ * @see <a href="http://modernroboticsinc.com/range-sensor">MR Range Sensor</a>
+ */
+/*@Autonomous(name = "Autoblue", group = "Competition")
+//@Disabled  // comment out or remove this line to enable this OpMode
+public class AutoBlue extends LinearOpMode {
+
+    ModernRoboticsI2cRangeSensor rangeSensor;
+
+    /*
+     * Declare Hardware
+     */
+
+  /*  // Wheels
+    private DcMotor WheelFrontLeft;
+    private DcMotor WheelFrontRight;
+    private DcMotor WheelBackLeft;
+    private DcMotor WheelBackRight;
+
+    // SlowMode Drive
+    private boolean slowModeDriveOn = true;
+    private boolean buttonSlowDriveIsPressed = false;
+    private final double SLOW_DRIVE = 0.4;
+    private final double FAST_DRIVE = 1.0; //0.9;
+    private double percentToSlowDrive = SLOW_DRIVE;
+
+    // SineDrive
+    private boolean sineDriveOn = true;
+    private boolean buttonSineIsPressed = false;
+    private double modifyBySine = Math.sin(Math.PI / 4);
+
+//   // REV Blinkin
+//   private RevBlinkinLedDriver LED;
+
+
+    @Override
+    public void init() {
+
+        // Initialize Wheels
+        telemetry.addData("I", "Initializing Wheels");
+        telemetry.update();
+
+        WheelFrontLeft = hardwareMap.dcMotor.get("WheelFL");
+        WheelFrontRight = hardwareMap.dcMotor.get("WheelFR");
+        WheelBackLeft = hardwareMap.dcMotor.get("WheelBL");
+        WheelBackRight = hardwareMap.dcMotor.get("WheelBR");
+
+        WheelFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        WheelFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        WheelBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        WheelBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        WheelFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        WheelFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        WheelBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        WheelBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        WheelFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        WheelFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        WheelBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        WheelBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        WheelFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        WheelFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        WheelBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        WheelBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+//      // Initialize REV Blinkin
+//      telemetry.addData("I", "Initializing Blinkin");
+//      telemetry.update();
+//
+//      LED = hardwareMap.get(RevBlinkinLedDriver.class, "LED");
+//      setLEDPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+
+
+        // Let the user know initialization is complete.
+        telemetry.addData("I", "Initialization Complete! :D");
+        telemetry.update();
+
+    }
+
+
+    @Override
+    public void runOpMode() {
+
+        // get a reference to our compass
+        rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "sensor_range");
+
+
+        while (opModeIsActive()) {
+            telemetry.addData("raw ultrasonic", rangeSensor.rawUltrasonic());
+            telemetry.addData("raw optical", rangeSensor.rawOptical());
+            telemetry.addData("cm optical", "%.2f cm", rangeSensor.cmOptical());
+            telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM));
+            telemetry.update();
+        }
+        double distance = rangeSensor.getDistance(DistanceUnit.CM);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // Drive Forward for 24 inches or until 10cm from something
+            _Drive.Straight(IDrive.Direction.FORWARD, 24, .5, 10);
+
+            // Turn 90 degrees to the left
+            _Drive.Left(90, .25);
+
+            // Drive Forward for 60 inches or until 10cm from something
+            _Drive.Straight(IDrive.Direction.FORWARD, 60, .5, 10);
+
+            // break will exit the loop for us
+            break;
+
+        }
+    }
+}
+*/
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/comp/Autozakk.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/comp/Autozakk.java
@@ -4,10 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Coach.DriveWithEncoders;
+import org.firstinspires.ftc.teamcode.comp.Autozakk;
 import org.firstinspires.ftc.teamcode.Interface.IDrive;
 
-@Autonomous(name = "Coach: AutoBlue002", group = "Coach")
-public class AutoBlue002 extends LinearOpMode {
+@Autonomous(name = "Autozakk", group = "Competition")
+public class Autozakk extends LinearOpMode {
 
     private IDrive _Drive;
 
@@ -24,13 +25,13 @@ public class AutoBlue002 extends LinearOpMode {
         while (opModeIsActive()) {
 
             // Drive Forward for 24 inches or until 10cm from something
-            _Drive.Straight(IDrive.Direction.FORWARD, 24, .5, 10);
+            _Drive.Straight(IDrive.Direction.FORWARD, 2,.5, 0);
 
-            // Turn 90 degrees to the left
+          /*// Turn 90 degrees to the left
             _Drive.Left(90, .25);
 
             // Drive Forward for 60 inches or until 10cm from something
-            _Drive.Straight(IDrive.Direction.FORWARD, 60, .5, 10);
+            _Drive.Straight(IDrive.Direction.FORWARD, 60, .5, 10);*/
 
             // break will exit the loop for us
             break;
