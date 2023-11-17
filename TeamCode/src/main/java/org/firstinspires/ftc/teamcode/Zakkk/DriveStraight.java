12a614eba4ac00f5beb7428b30851b883b544177
Obstagoon2 <Zakkljackson@hotmail.com>
2023-11-16 19:38:04 -0600

    drives straight perfect now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zakkk/DriveStraight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zakkk/DriveStraight.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.Zakkk;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -7,9 +10,13 @@ import org.firstinspires.ftc.teamcode.Coach.DriveWithEncoders;
 import org.firstinspires.ftc.teamcode.Interface.IDrive;
 
 @Autonomous(name = "DriveStraight", group = "competition")
+@Config
 public class DriveStraight extends LinearOpMode {
 
     private IDrive _Drive;
+    public static double DriveDistance = 24;
+    public static double Power = .25;
+    public static double StopDistance = 10;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -24,7 +31,13 @@ public class DriveStraight extends LinearOpMode {
         while (opModeIsActive()) {
 
             // Drive Forward for 24 inches or until 10cm from something
-            _Drive.Straight(IDrive.Direction.FORWARD, 24, .5, 10);
+            /*
+            [Blue/Red] [A=Crowd Side/B=Stage Side] [1=First Tile/2=Second Tile]
+            Blue A1 = DD = 1.75 / P = .18 | Turn 90D Left | DD = 106 / P = .25
+            Blue A2 = DD = 25.5 / P = .18 | Turn 90D Left | DD = 106 / P = .25
+
+             */
+            _Drive.Straight(IDrive.Direction.FORWARD, DriveDistance, Power, StopDistance);
             sleep(1000);
 
             // break will exit the loop for us
@@ -33,6 +46,8 @@ public class DriveStraight extends LinearOpMode {
     }
 
     private void Initialize(){
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
         // Initialize driving
         _Drive = new DriveWithEncoders(hardwareMap, telemetry);
     }
