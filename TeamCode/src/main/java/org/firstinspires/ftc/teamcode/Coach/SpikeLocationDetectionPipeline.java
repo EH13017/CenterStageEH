35e5e823da82b33506c3c0607d18784d36849a50
Chris <bankovic.chris@gmail.com>
2024-01-06 16:28:58 -0600

    Red Team Prop Complete

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Coach/SpikeLocationDetectionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Coach/SpikeLocationDetectionPipeline.java
@@ -19,32 +19,35 @@ public class SpikeLocationDetectionPipeline extends OpenCvPipeline {
 
    //public Scalar lowerRedCone = new Scalar(35.9, 149.2, 5.7);
    //public Scalar upperRedCone = new Scalar(150.2, 203.1, 255);
-   public Scalar lowerRedCone = new Scalar(35.9, 149.2, 5.7);
-   public Scalar upperRedCone = new Scalar(160, 203.1, 255);
-   public Scalar lowerBlueCone = new Scalar(0, 0, 136.0);
-   public Scalar upperBlueCone = new Scalar(255, 255, 194.1);
+   //Blue = 36, Green = 150, and Red = 5
+   public Scalar lowerRedCone = new Scalar(34,175.7, 32.6);
+   public Scalar upperRedCone = new Scalar(191.3,232.3, 174.3);
+   // public Scalar lowerBlueCone = new Scalar(0, 0, 136.0);
+   // public Scalar upperBlueCone = new Scalar(255, 255, 194.1);
+   public Scalar lowerBlueCone = new Scalar(0, 0, 0);
+   public Scalar upperBlueCone = new Scalar(0,0,0);
    int numRedCones = 0;
    int numBlueCones = 0;
    public double erodeWidth = 5;
    public double erodeHeight = 15;
    public double dilateWidth = 15;
    public double dilateHeight = 18;
-   public double areaLowThreshold = 50;
-   public double areaHighThreshold = 200;
+   // public double areaLowThreshold = 50;
+   // public double areaHighThreshold = 200;
    public AtomicInteger spikeLocation = new AtomicInteger(0);
    boolean enableTelemetry = true;
    boolean addDrawings = true;
 
-   public Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(120,0);
+   // public Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(120,0);
 
-   public int REGION_WIDTH = 80;
-   public int REGION_HEIGHT = 240;
-   Point region1_pointA = new Point(
-           REGION1_TOPLEFT_ANCHOR_POINT.x,
-           REGION1_TOPLEFT_ANCHOR_POINT.y);
-   Point region1_pointB = new Point(
-           REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-           REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+   // public int REGION_WIDTH = 80;
+   // public int REGION_HEIGHT = 240;
+   // Point region1_pointA = new Point(
+   //         REGION1_TOPLEFT_ANCHOR_POINT.x,
+   //         REGION1_TOPLEFT_ANCHOR_POINT.y);
+   // Point region1_pointB = new Point(
+   //         REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+   //         REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
    double detectedOffset = 0;
 
 
@@ -106,6 +109,10 @@ public class SpikeLocationDetectionPipeline extends OpenCvPipeline {
    public AtomicInteger getSpikeLocation(){
       return spikeLocation;
    }
+   double maxRectangleWidth = 0;
+   double maxRectX = 0;
+   double maxRectY = 0;
+   double maxRectangleCenter = 0;
 
    @Override
    public Mat processFrame(Mat input) {
@@ -124,108 +131,48 @@ public class SpikeLocationDetectionPipeline extends OpenCvPipeline {
 
       maskedInputMat.release();
 
+      List<MatOfPoint> theContours = new ArrayList<>();
+
       //determine biggest
-      if ( numRedCones > numBlueCones){
+      if ( numRedCones > numBlueCones){ // Red cones found!!!
          Imgproc.erode(redConeBinaryMat, redConeBinaryMat, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(erodeWidth, erodeHeight)));
          Imgproc.dilate(redConeBinaryMat, redConeBinaryMat, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(dilateWidth, dilateHeight)));
 
          Mat heirarchyMat = new Mat();
 
-         List<MatOfPoint> theContours = new ArrayList<>();
          Imgproc.findContours(redConeBinaryMat, theContours, heirarchyMat, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-         //Core.bitwise_and(input, input, maskedInputMat, redConeBinaryMat);
-         //Imgproc.drawContours(maskedInputMat, theContours, -1, new Scalar(0, 255, 0), 2);
-         Imgproc.drawContours(input, theContours, -1, new Scalar(0, 255, 0), 1);
 
-         //need to find how many red cones vs blue
-         List<Rect> theRectangles = new ArrayList<>();
-         double maxRectangleWidth = 0;
-         double maxRectX = 0;
-         double maxRectY = 0;
-         double maxRectangleCenter = 0;
+         theContours = trimContours(theContours);
 
-         if (theContours.size() > 0) {
-            for (int i = 0; i < theContours.size(); i++) {
-               Rect r = Imgproc.boundingRect(theContours.get(i));
-
-               double rectWidth = r.width;
-               //find the largest width
-               if (rectWidth > maxRectangleWidth){
-                  maxRectangleWidth = rectWidth;
-                  maxRectX = r.x;
-                  maxRectY = r.y;
-               }
-
-            }
-            maxRectangleCenter = maxRectangleWidth / 2;
-            detectedOffset = maxRectX + maxRectangleCenter - 160 ; //top left anchor point
-
-         } else {
-            detectedOffset = -1000;
-         }
-
-         setSpikeLocation(detectedOffset);
+         SettingSpikeLocation(theContours);
 
          if (addDrawings) {
-            Imgproc.drawContours(input, theContours, -1, new Scalar(0, 255, 0), 1);
-
+            drawBox(input, theContours, "Red");
             //draw a red line for the offset of the largest rectangle
             Imgproc.line(input, new Point(maxRectX + maxRectangleCenter, 0), new Point(maxRectX + maxRectangleCenter, 240), new Scalar(255, 0, 0));
-
          }
 
-      } else {
+      } else { // Blue cones found!
          Imgproc.erode(blueConeBinaryMat, blueConeBinaryMat, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(erodeWidth, erodeHeight)));
          Imgproc.dilate(blueConeBinaryMat, blueConeBinaryMat, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(dilateWidth, dilateHeight)));
 
          Mat heirarchyMat = new Mat();
 
-         List<MatOfPoint> theContours = new ArrayList<>();
+         //List<MatOfPoint> theContours = new ArrayList<>();
          Imgproc.findContours(blueConeBinaryMat, theContours, heirarchyMat, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-         //Core.bitwise_and(input, input, maskedInputMat, blueConeBinaryMat); //need to fix this to view cones
-         //Imgproc.drawContours(maskedInputMat, theContours, -1, new Scalar(0, 255, 0), 1);
-         Imgproc.drawContours(input, theContours, -1, new Scalar(0, 255, 0), 1);
 
+         theContours = trimContours(theContours);
 
-         List<Rect> theRectangles = new ArrayList<>();
-         double maxRectangleWidth = 0;
-         double maxRectX = 0;
-         double maxRectY = 0;
-         double maxRectangleCenter = 0;
-
-         if (theContours.size() > 0) {
-            for (int i = 0; i < theContours.size(); i++) {
-               Rect r = Imgproc.boundingRect(theContours.get(i));
-
-               double rectWidth = r.width;
-               //find the largest width
-               if (rectWidth > maxRectangleWidth){
-                  maxRectangleWidth = rectWidth;
-                  maxRectX = r.x;
-                  maxRectY = r.y;
-               }
-
-            }
-            maxRectangleCenter = maxRectangleWidth / 2;
-            detectedOffset = maxRectX + maxRectangleCenter - 160 ; //top left anchor point
-
-         } else {
-            detectedOffset = -1000;
-         }
-         setSpikeLocation(detectedOffset);
+         SettingSpikeLocation(theContours);
 
          if (addDrawings) {
-            Imgproc.drawContours(input, theContours, -1, new Scalar(0, 255, 0), 1);
+            drawBox(input, theContours, "Blue");
 
             //draw a red line for the offset of the largest rectangle
             Imgproc.line(input, new Point(maxRectX + maxRectangleCenter, 0), new Point(maxRectX + maxRectangleCenter, 240), new Scalar(255, 0, 0));
 
          }
       }
-
-      //find ratio to determine cone height
-
-
       /**
        * Add some nice and informative telemetry messages
        */
@@ -235,9 +182,40 @@ public class SpikeLocationDetectionPipeline extends OpenCvPipeline {
       telemetry.addData("numBlueCones", numBlueCones);
       telemetry.addData("detectedOffset", detectedOffset);
       telemetry.addData("spikeLocation", spikeLocation);
+      telemetry.addData("Cones", theContours.size());
       telemetry.update();
 
       return input;
+   }
+
+   void SettingSpikeLocation(List<MatOfPoint> theContours){
+      List<Rect> theRectangles = new ArrayList<>();
+      maxRectangleWidth = 0;
+      maxRectX = 0;
+      maxRectY = 0;
+      maxRectangleCenter = 0;
+
+      if (theContours.size() > 0) {
+         for (int i = 0; i < theContours.size(); i++) {
+            Rect r = Imgproc.boundingRect(theContours.get(i));
+
+            double rectWidth = r.width;
+            //find the largest width
+            if (rectWidth > maxRectangleWidth){
+               maxRectangleWidth = rectWidth;
+               maxRectX = r.x;
+               maxRectY = r.y;
+            }
+
+         }
+         maxRectangleCenter = maxRectangleWidth / 2;
+         detectedOffset = maxRectX + maxRectangleCenter - 160 ; //top left anchor point
+
+      } else {
+         detectedOffset = -1000;
+      }
+      setSpikeLocation(detectedOffset);
+
    }
 
    void setSpikeLocation(double offset){
@@ -247,12 +225,12 @@ public class SpikeLocationDetectionPipeline extends OpenCvPipeline {
       //1 = left, 2 = middle, 3 = right, -1 = unknown
       if (offset == -1000 ){
          spikeLoc = -1;
-      } else if (offset < -90){
-         spikeLoc = 1;
-      } else if (offset > 90){
-         spikeLoc = 3;
-      } else {
-         spikeLoc = 2;
+      } else if (offset < 90){   // Negative Number
+         spikeLoc = 1; // Left
+      } else if (offset > 900){  // Greater than 900
+         spikeLoc = 3; // Right
+      } else {                   // Between 0 and 900
+         spikeLoc = 2; // Center
       }
       spikeLocation.set(spikeLoc);
    }
@@ -269,6 +247,74 @@ public class SpikeLocationDetectionPipeline extends OpenCvPipeline {
          double scale = 1.25;
 
          Imgproc.putText(img, text, new Point(x, y), font_face, scale, color, 2, Imgproc.LINE_AA);
+      }
+   }
+
+   private List<MatOfPoint> trimContours(List<MatOfPoint> theContours) {
+      List<MatOfPoint> trimmedContours = new ArrayList<>();
+
+      for (MatOfPoint contour : theContours) {
+         // Get the bounding box for the contour
+         Rect boundingBox = Imgproc.boundingRect(contour);
+         if (IsValidBox(boundingBox.width, boundingBox.height)) {
+            trimmedContours.add(contour);
+         }
+      }
+
+      return trimmedContours;
+   }
+
+   private Boolean IsValidBox(int width, int height){
+      int minRight = 200;
+      int maxRight = 280;
+      int minLeft = 200;
+      int maxLeft = 280;
+      int minCenter = 120;
+      int maxCenter = 200;
+
+      if (  (width >= minRight  && width <= maxRight &&
+              height >= minRight && height <= maxRight) ||
+
+              (width >= minCenter  && width <= maxCenter &&
+                      height >= minCenter && height <= maxCenter) ||
+
+              (width >= minLeft  && width <= maxLeft &&
+                      height >= minLeft && height <= maxLeft)) {
+
+         return true;
+      }
+      return false;
+   }
+
+   void drawBox(Mat input, List<MatOfPoint> theContours, String color){
+
+      int fontFace = Imgproc.FONT_HERSHEY_SIMPLEX;
+      double fontScale = 1.0;
+      Scalar fontColor = new Scalar(255, 255, 255); // White color
+      int thickness = 2;
+      Point labelPosition = new Point(50, 50); // Adjust as needed
+
+      for (MatOfPoint contour : theContours) {
+         // Get the bounding box for the contour
+         Rect boundingBox = Imgproc.boundingRect(contour);
+
+         if (IsValidBox(boundingBox.width, boundingBox.height)) {
+            // Draw a rectangle around the contour
+            Imgproc.rectangle(input, boundingBox.tl(), boundingBox.br(), new Scalar(0, 255, 0), 1);
+
+            labelPosition = new Point(boundingBox.x, boundingBox.y);
+
+            String label = color + " " + spikeLocation;
+
+            // Put the label on the image
+            Imgproc.putText(input, label, labelPosition, fontFace, fontScale, fontColor, thickness);
+
+            telemetry.addData("Box",label);
+            telemetry.addData("bb.x + y         ", boundingBox.x + " | " + boundingBox.y);
+            telemetry.addData("bb.width + height", boundingBox.width + " | " + boundingBox.height);
+            telemetry.addData("","--------------------------------");
+            telemetry.addData("", "  ");
+         }
       }
    }
 
