3a03cb83965b96328a1f1f70bdfe921cc05440df
NinjaAdam <116517776+NinjaAdam@users.noreply.github.com>
2024-02-16 19:28:48 -0600

    I hate dark matter :|

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Adam/ClawTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Adam/ClawTest.java
@@ -12,6 +12,7 @@ public class ClawTest extends OpMode {
     boolean Touch;
     boolean toggle = false;
 
+
     @Override
     public void init() {
         Claw1 = hardwareMap.servo.get("Claw1");
