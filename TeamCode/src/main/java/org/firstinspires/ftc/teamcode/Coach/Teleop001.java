8181aedc08a5a498a39d6ded791ee27aeaaf7d12
Obione44 <charlie.lw.steele@gmail.com>
2024-02-27 20:48:09 -0600

    Autored comp is in progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Coach/Teleop001.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Coach/Teleop001.java
@@ -57,30 +57,46 @@ public class Teleop001 extends OpMode {
         telemetry.addData("I", "Initializing Wheels");
         telemetry.update();
 
-        WheelFrontLeft = hardwareMap.dcMotor.get("WheelFL");
-        WheelFrontRight = hardwareMap.dcMotor.get("WheelFR");
-        WheelBackLeft = hardwareMap.dcMotor.get("WheelBL");
-        WheelBackRight = hardwareMap.dcMotor.get("WheelBR");
 
-        WheelFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        WheelFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        WheelBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        WheelBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        WheelFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        WheelFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        WheelBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        WheelBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor WheelFrontLeft = hardwareMap.dcMotor.get("WheelFL");
+        DcMotor WheelBackLeft = hardwareMap.dcMotor.get("WheelBL");
+        DcMotor WheelFrontRight = hardwareMap.dcMotor.get("WheelFR");
+        DcMotor WheelBackRight = hardwareMap.dcMotor.get("WheelBR");
 
-        WheelFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
         WheelFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        WheelBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        WheelBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        WheelBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        WheelFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        WheelFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        WheelBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        WheelBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        WheelFrontLeft = hardwareMap.dcMotor.get("WheelFL");
+//        WheelFrontRight = hardwareMap.dcMotor.get("WheelFR");
+//        WheelBackLeft = hardwareMap.dcMotor.get("WheelBL");
+//        WheelBackRight = hardwareMap.dcMotor.get("WheelBR");
+//
+//        WheelFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        WheelFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        WheelBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        WheelBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//
+//        WheelFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        WheelFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        WheelBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        WheelBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//
+//        WheelFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+//        WheelFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+//        WheelBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+//        WheelBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+//
+//        WheelFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        WheelFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        WheelBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        WheelBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
         imu = hardwareMap.get(IMU.class, "imu");
@@ -163,6 +179,16 @@ public class Teleop001 extends OpMode {
         ToggleSlowModeDrive(oneButtonA);
 
         telemetry.update();
+
+        // Retrieve the IMU from the hardware map
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+
     }
 
 
@@ -175,27 +201,60 @@ public class Teleop001 extends OpMode {
     // Get the inputs from the controller for power [ PRO ]
     //******************************************************************
     private void ProMotorControl(double left_stick_y, double left_stick_x, double right_stick_x) {
-        double powerLeftY = -left_stick_y;   // DRIVE : Backward -1 <---> 1 Forward
-        double powerLeftX = left_stick_x*-1; // STRAFE:     Left -1 <---> 1 Right
-        double powerRightX = -right_stick_x; // ROTATE:     Left -1 <---> 1 Right
+//        double powerLeftY = -left_stick_y;   // DRIVE : Backward -1 <---> 1 Forward
+//        double powerLeftX = left_stick_x*-1; // STRAFE:     Left -1 <---> 1 Right
+//        double powerRightX = -right_stick_x; // ROTATE:     Left -1 <---> 1 Right
+//
+//        double r = Math.hypot(powerLeftX, powerLeftY);
+//        double robotAngle = Math.atan2(powerLeftY, powerLeftX) - Math.PI / 4;
+//        double leftX = powerRightX;
+//        final double v1 = r * Math.cos(robotAngle) / modifyBySine + leftX;
+//        final double v2 = r * Math.sin(robotAngle) / modifyBySine - leftX;
+//        final double v3 = r * Math.sin(robotAngle) / modifyBySine + leftX;
+//        final double v4 = r * Math.cos(robotAngle) / modifyBySine - leftX;
+//
+//        WheelFrontLeft.setPower(v1* percentToSlowDrive);
+//        WheelFrontRight.setPower(v2* percentToSlowDrive);
+//        WheelBackLeft.setPower(v3* percentToSlowDrive);
+//        WheelBackRight.setPower(v4* percentToSlowDrive);
+//
+//        telemetry.addData("Wheel Front Left",v1* percentToSlowDrive);
+//        telemetry.addData("Wheel Front Right",v2* percentToSlowDrive);
+//        telemetry.addData("Wheel Back Left",v3* percentToSlowDrive);
+//        telemetry.addData("Wheel Back Right",v4* percentToSlowDrive);
 
-        double r = Math.hypot(powerLeftX, powerLeftY);
-        double robotAngle = Math.atan2(powerLeftY, powerLeftX) - Math.PI / 4;
-        double leftX = powerRightX;
-        final double v1 = r * Math.cos(robotAngle) / modifyBySine + leftX;
-        final double v2 = r * Math.sin(robotAngle) / modifyBySine - leftX;
-        final double v3 = r * Math.sin(robotAngle) / modifyBySine + leftX;
-        final double v4 = r * Math.cos(robotAngle) / modifyBySine - leftX;
+        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
 
-        WheelFrontLeft.setPower(v1* percentToSlowDrive);
-        WheelFrontRight.setPower(v2* percentToSlowDrive);
-        WheelBackLeft.setPower(v3* percentToSlowDrive);
-        WheelBackRight.setPower(v4* percentToSlowDrive);
+        // This button choice was made so that it is hard to hit on accident,
+        // it can be freely changed based on preference.
+        // The equivalent button is start on Xbox-style controllers.
+        if (gamepad1.options) {
+            imu.resetYaw();
+        }
 
-        telemetry.addData("Wheel Front Left",v1* percentToSlowDrive);
-        telemetry.addData("Wheel Front Right",v2* percentToSlowDrive);
-        telemetry.addData("Wheel Back Left",v3* percentToSlowDrive);
-        telemetry.addData("Wheel Back Right",v4* percentToSlowDrive);
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        // Rotate the movement direction counter to the bot's rotation
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+        WheelFrontLeft.setPower(frontLeftPower);
+        WheelBackLeft.setPower(backLeftPower);
+        WheelFrontRight.setPower(frontRightPower);
+        WheelBackRight.setPower(backRightPower);
     }
 
     private void ToggleSlowModeDrive(boolean button) {
