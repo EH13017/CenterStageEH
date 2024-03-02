06d4b7a556ce9e43dbc4c4ea2ec076013fa59512
Obione44 <charlie.lw.steele@gmail.com>
2024-03-01 21:09:54 -0600

    Autored comp is in progress

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Charlie/Autoback.java
@@ -0,0 +1,189 @@
+package org.firstinspires.ftc.teamcode.Charlie;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Coach.DriveWithEncoders;
+import org.firstinspires.ftc.teamcode.Interface.IDrive;
+
+@Autonomous(name = "Charlie: Autoback", group = "Charlie")
+public class Autoback extends LinearOpMode {
+
+    private IDrive _Drive;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Initialize();
+
+        telemetry.addData("=D", "Ready to start!");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // Drive Forward for 24 inches or until 10cm from something
+            _Drive.Straight(IDrive.Direction.BACKWARD, 24, .5, 10);
+
+            // Turn 90 degrees to the left
+            _Drive.Left(90, .25);
+
+            // Reverse intake to push out pixels
+            _Drive.Intake(IDrive.Direction.BACKWARD,4000,.35);
+
+            // Drive Forward for 60 inches or until 10cm from something
+//            _Drive.Straight(IDrive.Direction.FORWARD, 60, .5, 10);
+
+            // break will exit the loop for us
+            break;
+        }
+
+        while (opModeIsActive()) {
+
+            // Drive Forward for 24 inches or until 10cm from something
+            _Drive.Straight(IDrive.Direction.BACKWARD, 24, .5, 10);
+
+
+            // Reverse intake to push out pixels
+            _Drive.Intake(IDrive.Direction.BACKWARD,4000,.35);
+
+            // Turn 90 degrees to the left
+            _Drive.Left(90, .25);
+
+
+            // Drive Forward for 60 inches or until 10cm from something
+//            _Drive.Straight(IDrive.Direction.FORWARD, 60, .5, 10);
+
+            // break will exit the loop for us
+            break;
+        }
+
+
+
+
+
+
+
+
+
+
+        while (opModeIsActive()) {
+
+            // Drive Forward for 24 inches or until 10cm from something
+            _Drive.Straight(IDrive.Direction.BACKWARD, 24, .5, 10);
+
+            // Turn 90 degrees to the left
+            _Drive.Left(270, .25);
+
+            // Reverse intake to push out pixels
+            _Drive.Intake(IDrive.Direction.BACKWARD,4000,.35);
+
+
+
+            // Drive Forward for 60 inches or until 10cm from something
+//            _Drive.Straight(IDrive.Direction.FORWARD, 60, .5, 10);
+
+            // break will exit the loop for us
+            break;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        while (opModeIsActive()) {
+
+            // Drive Forward for 24 inches or until 10cm from something
+            _Drive.Straight(IDrive.Direction.BACKWARD, 24, .5, 10);
+
+            // Turn 90 degrees to the left
+            _Drive.Left(90, .25);
+
+            // Reverse intake to push out pixels
+            _Drive.Intake(IDrive.Direction.BACKWARD,4000,.35);
+
+            // Drive Forward for 60 inches or until 10cm from something
+//            _Drive.Straight(IDrive.Direction.FORWARD, 60, .5, 10);
+
+            // break will exit the loop for us
+            break;
+        }
+
+        while (opModeIsActive()) {
+
+            // Drive Forward for 24 inches or until 10cm from something
+            _Drive.Straight(IDrive.Direction.BACKWARD, 24, .5, 10);
+
+
+            // Reverse intake to push out pixels
+            _Drive.Intake(IDrive.Direction.BACKWARD,4000,.35);
+
+            // Turn 90 degrees to the left
+            _Drive.Left(90, .25);
+
+
+            // Drive Forward for 60 inches or until 10cm from something
+//            _Drive.Straight(IDrive.Direction.FORWARD, 60, .5, 10);
+
+            // break will exit the loop for us
+            break;
+        }
+
+
+
+
+
+
+
+
+
+
+        while (opModeIsActive()) {
+
+            // Drive Forward for 24 inches or until 10cm from something
+            _Drive.Straight(IDrive.Direction.BACKWARD, 24, .5, 10);
+
+            // Turn 90 degrees to the left
+            _Drive.Left(270, .25);
+
+            // Reverse intake to push out pixels
+            _Drive.Intake(IDrive.Direction.BACKWARD,4000,.35);
+
+
+
+            // Drive Forward for 60 inches or until 10cm from something
+//            _Drive.Straight(IDrive.Direction.FORWARD, 60, .5, 10);
+
+            // break will exit the loop for us
+            break;
+        }
+    }
+
+    private void Initialize(){
+        // Initialize driving
+        _Drive = new DriveWithEncoders(hardwareMap, telemetry);
+    }
+}
+
+
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Charlie/Autobackblueright.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Charlie/Autobackblueright.java
@@ -1,13 +1,14 @@
-package org.firstinspires.ftc.teamcode.Coach;
+package org.firstinspires.ftc.teamcode.Charlie;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Coach.DriveWithEncoders;
 import org.firstinspires.ftc.teamcode.Interface.IDrive;
 
-@Autonomous(name = "Coach: AutoBlue001", group = "Coach")
-public class AutoBlue001 extends LinearOpMode {
+@Autonomous(name = "Charlie: Autobackblueright", group = "Charlie")
+public class Autobackblueright extends LinearOpMode {
 
     private IDrive _Drive;
 
@@ -30,10 +31,10 @@ public class AutoBlue001 extends LinearOpMode {
             _Drive.Left(90, .3);
 
             // Reverse intake to push out pixels
-            _Drive.Intake(IDrive.Direction.BACKWARD, 4000, .35);
+            _Drive.Intake(IDrive.Direction.BACKWARD, 4000, .5);
 
             // Drive Forward for 60 inches or until 10cm from something
-//            _Drive.Straight(IDrive.Direction.FORWARD, 60, .5, 10);
+            _Drive.Straight(IDrive.Direction.FORWARD, 60, .5, 10);
 
             // break will exit the loop for us
             break;
